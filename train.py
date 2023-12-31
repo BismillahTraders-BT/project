1b00a96079304d6e9ddb0d31c51b9e3ebd8c6cdb
waleedbinosama <waleedbinosama@gmail.com>
2024-01-01 02:34:42 +0500

    changes to main

--- train.py
+++ train.py
@@ -4,8 +4,47 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
 import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+# Load the data from the uploaded CSV file
+file_path = 'data/dummy_sensor_data.csv'
+df = pd.read_csv(file_path)
+
+# Display the first few rows of the dataframe to understand its structure
+##print(df.head())
+
+# Convert 'Timestamp' to a numeric format (Unix timestamp)
+df['Timestamp'] = pd.to_datetime(df['Timestamp']).astype('int64') // 10**9  # Use // for integer division
+
+# Separate features and target
+X = df.drop('Reading', axis=1)
+y = df['Reading']
+
+# Define a column transformer that will one-hot encode categorical variables
+# and scale numerical features
+column_transformer = ColumnTransformer(
+    transformers=[
+        ('onehot', OneHotEncoder(), ['Machine_ID', 'Sensor_ID']),
+        ('scaler', StandardScaler(), ['Timestamp'])
+    ],
+    remainder='passthrough'
+)
+
+# Apply the transformations
+X_transformed = column_transformer.fit_transform(X)
+
+# Split the transformed data into training and validation sets
+X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, test_size=0.2, random_state=42)
+
+# Display the shapes of the training and validation sets
+print(f"X_train shape: {X_train.shape}, X_val shape: {X_val.shape}")
+print(f"y_train shape: {y_train.shape}, y_val shape: {y_val.shape}")
+
 
 model = RandomForestRegressor(random_state=42, n_estimators=100, max_depth=10)
 
